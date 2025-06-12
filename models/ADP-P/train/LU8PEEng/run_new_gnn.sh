9fe89e018b8f3565053dc87938e44ae1fbf27810
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2025-06-12 12:18:10 +0800

    Add files via upload

---
+++ models/ADP-P/train/LU8PEEng/run_new_gnn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+learning_rates=(0.001)
+
+decays=(0.001)
+
+neurons=(256)
+
+batches=(32)
+
+num_layers=(4 3 2)
+
+conv_types=('SAGE')
+
+dataset_dir={Your_dataset_director}
+
+for lr in "${learning_rates[@]}"; do
+    for decay in "${decays[@]}"; do
+			for neuron in "${neurons[@]}"; do
+				for batch in "${batches[@]}";do
+					for num_layer in "${num_layers[@]}"; do
+						for conv_type in "${conv_types[@]}"; do
+							
+							nohup_cmd="nohup bsub -I python GAT_inst_new.py --lr $lr --decay $decay --neurons $neuron --batch $batch --num_layers $num_layer --conv_type $conv_type --dataset_dir $dataset_dir > \"${job_name}.out\" 2> \"${job_name}.err\" &"
+                            echo "$nohup_cmd"
+							eval $nohup_cmd
+						done
+					done
+				done
+        done
+    done
+done
+
+echo "All jobs have been submitted."
