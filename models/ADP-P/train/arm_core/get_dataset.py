5255937cf2dfb3085bf786cc3db1ae34d79bc03c
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2025-06-12 11:51:18 +0800

    Create get_dataset.py

---
+++ models/ADP-P/train/arm_core/get_dataset.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+import torch
+from torch_geometric.data import Data, InMemoryDataset
+from tqdm import tqdm
+from extract_xml import *
+from get_segment import *
+from path_count import *
+
+class GNNDataset_criticalpath(InMemoryDataset): 
+    def __init__(self, root, type='', transform=None, pre_transform=None):
+        self.type = type
+        super(GNNDataset_criticalpath, self).__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return os.listdir('/home/wllpro/llwang/yfdai/HRAE_paper/raw_crit_paths/arm_core/')  
+
+    @property
+    def processed_file_names(self):
+        if self.type == 'wo_inter':
+            return ['arm_core_wo_inter.pt']
+        elif self.type == 'rrg':
+            return ['arm_core_rrg.pt']
+        elif self.type == 'clb':
+            return ['arm_core_clb.pt']
+        else:
+            return ['arm_core.pt']
+        
+    def download(self):
+        pass
+
+    def process(self):
+        report_df = pd.read_csv('/home/wllpro/llwang/yfdai/HRAE_paper/raw_dataset_results/arm_core.csv')
+        data_list = []
+
+        for json_file in tqdm(self.raw_file_names, desc='Processing JSON Files'):
+            if json_file.endswith('.json'):
+                json_file_path = f'/home/wllpro/llwang/yfdai/HRAE_paper/raw_crit_paths/arm_core/{json_file}'
+                arch_name = json_file.replace('.json', '')
+               
+                if f'{arch_name}.xml' in os.listdir('/home/wllpro/llwang/yfdai/HRAE_paper/raw_dataset_archs/'):
+                    label_row = report_df[report_df['Architecture'] == arch_name]
+                    if (not label_row.empty and not pd.isna(label_row['Critical Path'].iloc[0])):
+                        # get average_improvement as a label
+                        label = torch.tensor([label_row['Critical Path'].iloc[0] * label_row['Total Routing Area'].iloc[0]], dtype=torch.float) * 1e-7
+                        # read JSON file
+                        node_labels = count_node_names(json_file_path)
+
+                        xml_file_path = f'/home/wllpro/llwang/yfdai/HRAE_paper/raw_dataset_archs/{arch_name}.xml'
+                        data = process_xml_to_data(xml_file_path, label, node_labels)
+                        data_list.append(data)
+        
+        print(data_list)
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
+if __name__ == '__main__':
+    dataset = GNNDataset_criticalpath(root='/home/wllpro/llwang/yfdai/HRAE_paper/final_dataset')
+
+# Print information about the first 20 graphs in the dataset
+# for graph_data in dataset[:10]:
+#     edge_sample = graph_data.edge_index[:, 0]
+#     node_sample = graph_data.x[100:200]
+#     label = graph_data.y
+#     print(f'Edge Sample: {edge_sample}, Node Sample: {node_sample}, Label: {label}')
