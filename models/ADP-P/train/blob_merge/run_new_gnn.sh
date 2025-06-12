a6f8b5fd2fb7b7be9bd222f3fe0df194c2b10d60
Wang Yuanqi <101231450+Wang-Yuanqi-source@users.noreply.github.com>
2025-06-12 11:40:27 +0800

    Update run_new_gnn.sh

--- models/ADP-P/train/blob_merge/run_new_gnn.sh
+++ models/ADP-P/train/blob_merge/run_new_gnn.sh
@@ -12,6 +12,8 @@ num_layers=(4 3 2)
 
 conv_types=('SAGE')
 
+dataset_dir={Your_dataset_director}
+
 for lr in "${learning_rates[@]}"; do
     for decay in "${decays[@]}"; do
 			for neuron in "${neurons[@]}"; do
@@ -19,7 +21,7 @@ for lr in "${learning_rates[@]}"; do
 					for num_layer in "${num_layers[@]}"; do
 						for conv_type in "${conv_types[@]}"; do
 							# 构建bsub提交命令
-							nohup_cmd="nohup bsub -I python GAT_inst_new.py --lr $lr --decay $decay --neurons $neuron --batch $batch --num_layers $num_layer --conv_type $conv_type > \"${job_name}.out\" 2> \"${job_name}.err\" &"
+							nohup_cmd="nohup bsub -I python GAT_inst_new.py --lr $lr --decay $decay --neurons $neuron --batch $batch --num_layers $num_layer --conv_type $conv_type --dataset_dir $dataset_dir > \"${job_name}.out\" 2> \"${job_name}.err\" &"
                             echo "$nohup_cmd"
 							eval $nohup_cmd
 						done
